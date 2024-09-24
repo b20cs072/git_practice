75e6f019942d3fd1d2cca874fbce4744ecec456d
b20cs072 <b20cs072@mace.ac.in>
2024-09-24 10:38:32 +0530

    changed Hello to Hello Planet

--- src/main/java/com/jthread/regapp/SecondClass.java
+++ src/main/java/com/jthread/regapp/SecondClass.java
@@ -2,6 +2,6 @@ package com.jthread.regapp;
 
 public class SecondClass {
     public static void print() {
-        System.out.println("Hello");
+        System.out.println("Hello Planet");
     }
 }
